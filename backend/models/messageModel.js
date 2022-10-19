7fc44521ce1f9ea0aabd52fd2311d2f27edb61d8
Haruna Oseni <harunaoseni23@gmail.com>
2022-10-19 13:41:42 -0500

    add: message model schema code

---
+++ backend/models/messageModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+message.exports = mongoose.model("Message", messageSchema);
