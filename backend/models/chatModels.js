3719b63d6fd58e1cd65a497f12f5b40782b12517
Haruna Oseni <harunaoseni23@gmail.com>
2022-10-19 13:41:33 -0500

    add: chat model schema

---
+++ backend/models/chatModels.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  },
+  { timestamps: true }
+);
