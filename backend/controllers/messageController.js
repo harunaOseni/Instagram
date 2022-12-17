26dca99e668d86158b44ab570e91d2f99cdff244
Haruna Oseni <harunaoseni23@gmail.com>
2022-12-16 19:29:34 -0600

    add: sending new message controller added

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,27 @@
+const catchAsync = require("../middlewares/catchAsync");
+const Message = require("../models/messageModel");
+const Chat = require("../models/chatModel");
+
+// Send New Message
+exports.newMessage = catchAsync(async (req, res, next) => {
+  const { chatId, content } = req.body;
+
+  const msgData = {
+    sender: req.user._id,
+    chatId,
+    content,
+  };
+
+  const newMessage = await Message.create(msgData);
+
+  await Chat.findByIdAndUpdate(chatId, {
+    latestMessage: newMessage,
+  });
+
+  res.status(200).json({
+    success: true,
+    newMessage,
+  });
+});
+
+
