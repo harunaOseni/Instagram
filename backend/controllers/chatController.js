2f5533205b30e31c152d9777e9f734ad8be41706
Haruna Oseni <harunaoseni23@gmail.com>
2022-12-16 19:18:00 -0600

    add: check if chat exist new chat route

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -2,3 +2,13 @@ const catchAsync = require("../middlewares/catchAsync");
 const Chat = require("../models/chatModel");
 
 // Create a new chat
+exports.newChat = catchAsync(async (req, res, next) => {
+  const { receiverId } = req.body;
+  const { _id } = req.user;
+
+  const chatExists = await Chat.findOne({
+    users: {
+        $all: [_id, receiverId],
+    }
+  });
+});
