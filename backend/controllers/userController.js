233d0f2d2929c6a7e5f4ff4f285f3b6b97e36f3f
Haruna Oseni <harunaoseni23@gmail.com>
2022-12-17 21:10:34 -0600

    add: delete profile function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -208,3 +208,39 @@ exports.updateProfile = catchAsync(async (req, res, next) => {
     success: true,
   });
 });
+
+// Delete Profile
+exports.deleteProfile = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  const posts = user.posts;
+  const followers = user.followers;
+  const following = user.following;
+  const userId = user._id;
+
+  // delete post & user images
+
+  await user.remove();
+
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  for (let i = 0; i < posts.length; i++) {
+    const post = await Post.findById(posts[i]);
+    await post.remove();
+  }
+
+  for (let i = 0; i < followers.length; i++) {
+    const follower = await User.findById(followers[i]);
+
+    const index = follower.following.indexOf(userId);
+    follower.following.splice(index, 1);
+    await follower.save();
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Deleted",
+  });
+});
