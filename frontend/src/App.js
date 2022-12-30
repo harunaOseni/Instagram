6b1ec969d1902acaccfe195c3132d21711a4e59e
Haruna Oseni <harunaoseni23@gmail.com>
2022-12-30 16:19:23 -0600

    add: app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { lazy, Suspense, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import { toast } from "react-toastify";
+import Header from "./components/Navbar/Header";
 
 const Login = lazy(() => import("./components/User/Login"));
 const SignUp = lazy(() => import("./components/User/SignUp"));
+const ForgotPassword = lazy(() => import("./components/User/ForgotPassword"));
+const ResetPassword = lazy(() => import("./components/User/ResetPassword"));
+const Profile = lazy(() => import("./components/User/Profile"));
 
 function App() {
   return (
     <>
-      <SignUp />
+      <Header />
+      <Profile />
     </>
   );
 }
