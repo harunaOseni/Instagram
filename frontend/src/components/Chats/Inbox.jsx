31793abbdae0cd32a5a5caf1779e8a05b727ffcf
Haruna Oseni <harunaoseni23@gmail.com>
2023-01-02 18:24:23 -0600

    add: inbox component

---
+++ frontend/src/components/Chats/Inbox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Inbox = () => {
+    return (
+        <>
+            <div className="mt-14 sm:mt-[4.7rem] pb-4 rounded ">
+
+            </div>
+        </>
+    )
+}; 
+
+export default Inbox;
