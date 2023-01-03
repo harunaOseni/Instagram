bb8281128d6db8c86052532ca4c6ec9bcf3ccfec
Haruna Oseni <harunaoseni23@gmail.com>
2023-01-02 18:20:56 -0600

    add: Inbox component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,25 @@ import TextField from "@mui/material/TextField";
 import { toast } from "react-toastify";
 import Header from "./components/Navbar/Header";
 import UpdateProfile from "./components/User/Update/UpdateProfile";
+import UpdatePassword from "./components/User/Update/UpdatePassword";
 
+const Home = lazy(() => import("./components/Home/Home"));
 const Login = lazy(() => import("./components/User/Login"));
 const SignUp = lazy(() => import("./components/User/SignUp"));
 const ForgotPassword = lazy(() => import("./components/User/ForgotPassword"));
 const ResetPassword = lazy(() => import("./components/User/ResetPassword"));
 const Profile = lazy(() => import("./components/User/Profile"));
 const Update = lazy(() => import("./components/User/Update/Update"));
-
+const Inbox = lazy(() => import("./components/Chats/Inbox"));
 
 function App() {
   return (
     <>
       <Header />
-      <Update>
-        <UpdateProfile />
-      </Update>
+      {/* <Update>
+        <UpdatePassword />
+      </Update> */}
+      <Inbox />
     </>
   );
 }
