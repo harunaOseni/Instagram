4750a336b9bfb00e5f3aa0786d9d741dcde9122a
Haruna Oseni <harunaoseni23@gmail.com>
2023-01-04 04:00:29 -0600

    add: routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import { toast } from "react-toastify";
 import Header from "./components/Navbar/Header";
 import UpdateProfile from "./components/User/Update/UpdateProfile";
 import UpdatePassword from "./components/User/Update/UpdatePassword";
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+
 
 const Home = lazy(() => import("./components/Home/Home"));
 const Login = lazy(() => import("./components/User/Login"));
@@ -14,15 +16,37 @@ const ResetPassword = lazy(() => import("./components/User/ResetPassword"));
 const Profile = lazy(() => import("./components/User/Profile"));
 const Update = lazy(() => import("./components/User/Update/Update"));
 const Inbox = lazy(() => import("./components/Chats/Inbox"));
+const NotFound = lazy(() => import("./components/Errors/NotFound"));
 
 function App() {
   return (
     <>
-      <Header />
-      {/* <Update>
-        <UpdatePassword />
-      </Update> */}
-      <Inbox />
+      {/* <Header /> */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/password/forgot" element={<ForgotPassword />} />
+            <Route path="/password/reset/:token" element={<ResetPassword />} />
+            <Route path="/:username" element={<Profile />} />
+            <Route path="/accounts/edit" element={
+              <Update>
+                <UpdateProfile />
+              </Update>
+            }
+            />
+            <Route path="/accounts/password/change" element={
+              <Update>
+                <UpdatePassword />
+              </Update>
+            }
+            />
+            <Route path="/direct/inbox" element={<Inbox />} />
+            <Route path="/direct/t/:chatId/:userId" element={<Inbox />} />
+            <Route path="/error" element={<NotFound />} />
+          </Routes>
+      </Router>
     </>
   );
 }
