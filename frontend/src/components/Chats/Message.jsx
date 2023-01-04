b0d32e9df0463a82e7100fe00bfad9e0a37c0274
Haruna Oseni <harunaoseni23@gmail.com>
2023-01-04 04:01:24 -0600

    add: message component

--- frontend/src/components/Chats/Message.jsx
+++ frontend/src/components/Chats/Message.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Message = () => {}; 
+const Message = () => {
+  return (
+    <span className="self-end text-sm text-white bg-violet-600 px-4 py-3 rounded-3xl max-w-xs">
+      Hey man thinking of a collaboration
+    </span>
+  );
+};
 
 export default Message;
